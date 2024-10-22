57d1ed765f3299ac8e67ae2ddba004906e7558ce
mkmahmud <mahmudulhasanmk434@gmail.com>
2024-10-22 18:01:30 +0600

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div> 
-      <h1>Hello MK</h1>
+      
+      <Button className="">Click me</Button>
     </div>
       );
 }
